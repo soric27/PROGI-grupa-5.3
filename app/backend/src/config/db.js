aac28379e629db18f3f136d056984ee4873e0321
Domagoj <dv56179@fer.hr>
2025-11-12 05:16:30 +0100

    Pripream backenda za Render

--- app/backend/src/config/db.js
+++ app/backend/src/config/db.js
@@ -14,5 +14,6 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
 
